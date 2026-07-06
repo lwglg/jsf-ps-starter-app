3a8a07a83f80de9089890ba55532e283282fb59e
Guilherme Lima Gonçalves <lwglguilherme@gmail.com>
2026-07-06 17:58:49 -0300

    feat: create app boilerplate structure

---
+++ jsfstarterapp/src/main/java/com/rsdata/App.java
@@ -0,0 +1,13 @@
+package com.rsdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
