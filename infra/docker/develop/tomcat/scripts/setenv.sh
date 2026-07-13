241238eb0d8ef9ef268b8cc77bdc44e3454da31e
Guilherme Lima Gonçalves <lwglguilherme@gmail.com>
2026-07-13 17:42:39 -0300

    feat: implement project stage override for custom fallback pages

--- infra/docker/develop/tomcat/scripts/setenv.sh
+++ infra/docker/develop/tomcat/scripts/setenv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-export CATALINA_OPTS="-Ddb.hostname=${DB_HOSTNAME} -Ddb.username=${DB_USERNAME} -Ddb.password=${DB_PASSWORD} -Ddb.port=${DB_PORT} -Ddb.name=${DB_NAME} -Xms512m -Xmx1024m"
+export CATALINA_OPTS="$CATALINA_OPTS -Djakarta.faces.PROJECT_STAGE=${APP_ENVIRONMENT} -Ddb.hostname=${DB_HOSTNAME} -Ddb.username=${DB_USERNAME} -Ddb.password=${DB_PASSWORD} -Ddb.port=${DB_PORT} -Ddb.name=${DB_NAME} -Xms512m -Xmx1024m"
 export JVM_ROUTE="tomcat_node_1"
